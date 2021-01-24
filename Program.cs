04386a725efeaab142ab8ee7a9dc6863e8c96079
Tuğba <74349907+tubaaktas@users.noreply.github.com>
2021-01-24 22:58:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DictionaryKavram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> CustomerNameCustomerNumber = new Dictionary<string, int>();
+          
+            CustomerNameCustomerNumber.Add("Tuğba", 1995);
+            CustomerNameCustomerNumber.Add("Tolgay", 1999);
+            CustomerNameCustomerNumber.Add("Zeyt", 1997);
+            CustomerNameCustomerNumber.Add("İdil", 1998);
+            CustomerNameCustomerNumber.Add("Sefa", 2000);
+
+            foreach (var DegerOku in CustomerNameCustomerNumber)
+            {
+                Console.WriteLine(DegerOku);
+            }
+            Console.ReadLine();
+        }
+    }
+}
